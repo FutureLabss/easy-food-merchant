58c34f2fe3188a9350636abcae13a31a5899045c
titus ukpono <titusukpono0@gmail.com>
2023-05-24 10:46:29 +0100

    merchant

--- src/hooks/query/product.tsx
+++ src/hooks/query/product.tsx
@@ -1,3 +1,12 @@
-export  function useProduct(){
+// import { IQueryArgs } from "@/lib/interfaces/query";
+// import {IOrder} from "../../lib/interfaces/orders"
+// import { getAllOrderApi} from "@/pages/api/services/order";
+// import { useMerchantQuery } from "../helpers/query";
 
-}
+// export  function useProduct(){
+//     const queryObject: IQueryArgs<string, IOrder[]> = {
+//         key: ["order"],
+//         callback: getAllOrderApi,
+//       };
+//       return useMerchantQuery(queryObject);
+// }
